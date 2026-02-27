4301c8f0dcb6a1e521bc2d4bbb64d4b04cc00db1
Dev-Victor-Nunes <arthurpessoadasilva192@gmail.com>
2026-02-27 12:24:19 -0300

    atualização da tela de login

--- login.js
+++ login.js
@@ -1,3 +1,16 @@
-const home = () => {
+function home() {
+    const email = document.getElementById("email").value.trim();
+    const senha = document.getElementById("senha").value.trim();
+    const mensagem = document.getElementById("mensagemErro");
+
+    mensagem.classList.remove("mostrar");
+    mensagem.innerText = "";
+
+    if (email === "" || senha === "") {
+        mensagem.innerText = "Preencha todos os campos!";
+        mensagem.classList.add("mostrar"); 
+        return;
+    }
+
     window.location.href = "home.html";
 }
